22bccf01fe2108c68f82fc7071677d3213064fe5
cris-oenning <crisoenning@gmail.com>
2023-09-20 15:18:23 -0300

    feat: finalização parte 1

--- src/components/SectionServices.tsx
+++ src/components/SectionServices.tsx
@@ -5,10 +5,11 @@ import PhoneIcon from '@/assets/icon-phone.svg'
 import SoluctionsIcon from '@/assets/icon-solutions.svg'
 import OptionIcon from '@/assets/icon-options.svg'
 import CardIcon from '@/assets/icon-card.svg'
+import ImagePhone from '@/assets/phone.png'
 
 export function SectionServices() {
   return (
-    <section className="w-full h-[965px]">
+    <section className="relative w-full h-[965px] ">
       <Container>
         <div className="flex-1 max-w-[594px] pt-32">
           <span className="block text-primary-orange text-sm font-bold uppercase mb-9">serviços exclusivos</span>
@@ -42,7 +43,7 @@ export function SectionServices() {
               </div>
               <p className="flex-1 text-txt-gray pr-2">Diversas opções de investimentos, de acordo com o seu perfil de investidor</p>
             </li>
-            <li className="flex items-center gap-9 pb-9 border-b-[1px] border-opacity-gray">
+            <li className="flex items-center gap-9">
               <div className="w-7 h-7 flex items-center justify-center">
                 <Image
                   src={CardIcon}
@@ -54,6 +55,9 @@ export function SectionServices() {
           </ul>
         </div>
       </Container>
+      <div className="absolute top-0 flex items-center right-0 w-[32%] h-full bg-gray-phone">
+        <Image src={ImagePhone} alt="Phone" className="translate-x-[-50%]" />
+      </div>
     </section>
   )
 }
